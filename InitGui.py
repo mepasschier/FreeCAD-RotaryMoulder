1c604a0b9379c59f6bf0e68bad20e4317d18a8ba
Mike Passchier <m.e.passchier@gmail.com>
2026-05-20 14:58:18 +0200

    Update InitGui.py

--- InitGui.py
+++ InitGui.py
@@ -1,44 +1,10 @@
 # -*- coding: utf-8 -*-
-# Rotary Moulder Workbench for FreeCAD
-import os
-import FreeCAD
-import FreeCADGui
+"""Rotary Moulder Workbench loader for FreeCAD.
 
-ADDON_DIR = os.path.dirname(__file__)
-ICON_DIR = os.path.join(ADDON_DIR, "Resources", "icons")
-
-
-class RotaryMoulderWorkbench(FreeCADGui.Workbench):
-    """Workbench for designing rotary cookie moulder drums."""
-
-    MenuText = "Rotary Moulder"
-    ToolTip = "Design rotary cookie moulders with drafted cavities"
-    Icon = os.path.join(ICON_DIR, "RotaryMoulder.svg")
-
-    def Initialize(self):
-        # Import commands so they register with FreeCADGui
-        from freecad.rotary_moulder import commands  # noqa: F401
-
-        cmds = [
-            "RotaryMoulder_CreateDrum",
-            "RotaryMoulder_AddCavityFromSketch",
-            "RotaryMoulder_AddDetail",
-            "RotaryMoulder_AddDockers",
-            "RotaryMoulder_ImportShape",
-            "RotaryMoulder_PatternCavities",
-            "RotaryMoulder_ToggleDebug",
-        ]
-        self.appendToolbar("Rotary Moulder", cmds)
-        self.appendMenu("Rotary Moulder", cmds)
-
-    def Activated(self):
-        FreeCAD.Console.PrintMessage("Rotary Moulder workbench activated.\n")
-
-    def Deactivated(self):
-        FreeCAD.Console.PrintMessage("Rotary Moulder workbench deactivated.\n")
-
-    def GetClassName(self):
-        return "Gui::PythonWorkbench"
-
-
-FreeCADGui.addWorkbench(RotaryMoulderWorkbench())
+This file is required by FreeCAD to recognize the directory as a
+workbench. The actual workbench class and command registration happen
+in freecad/rotary_moulder/__init__.py (which is auto-loaded by
+FreeCAD's namespace package discovery for ``freecad.*``).
+"""
+# Trigger the package __init__.py which registers the workbench.
+from freecad import rotary_moulder  # noqa: F401
