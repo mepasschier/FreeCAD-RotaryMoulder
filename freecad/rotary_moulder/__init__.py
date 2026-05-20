5132405d1e5d349649e30c7cc6b41cb1fd424e3d
Mike Passchier <m.e.passchier@gmail.com>
2026-05-20 14:57:24 +0200

    Update __init__.py

--- freecad/rotary_moulder/__init__.py
+++ freecad/rotary_moulder/__init__.py
@@ -2,19 +2,27 @@
 """Rotary Moulder Workbench package."""
 import os
 import FreeCAD
+
 __version__ = "1.1.0"
-ADDON_DIR = os.path.join(
-    FreeCAD.getUserAppDataDir(), "Mod", "RotaryMoulder"
-)
+
+# Find the addon root. This file lives at <addon>/freecad/rotary_moulder/__init__.py
+# so two parents up gets us to the addon root, regardless of whether the
+# addon folder is named "RotaryMoulder", "FreeCAD-RotaryMoulder", or
+# anything else (Addon Manager installs may use different names).
+ADDON_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.abspath(__file__))))
 ICON_PATH = os.path.join(ADDON_DIR, "Resources", "icons",
                          "RotaryMoulder.svg")
+
 if FreeCAD.GuiUp:
     import FreeCADGui
+
     class RotaryMoulderWorkbench(FreeCADGui.Workbench):
         """Workbench for designing rotary cookie moulder drums."""
         MenuText = "Rotary Moulder"
         ToolTip = "Design rotary cookie moulders with drafted cavities"
         Icon = ICON_PATH
+
         def Initialize(self):
             from freecad.rotary_moulder import commands  # noqa: F401
             cmds = [
@@ -28,14 +36,18 @@ if FreeCAD.GuiUp:
             ]
             self.appendToolbar("Rotary Moulder", cmds)
             self.appendMenu("Rotary Moulder", cmds)
+
         def Activated(self):
             FreeCAD.Console.PrintMessage(
                 "Rotary Moulder workbench activated.\n"
             )
+
         def Deactivated(self):
             FreeCAD.Console.PrintMessage(
                 "Rotary Moulder workbench deactivated.\n"
             )
+
         def GetClassName(self):
             return "Gui::PythonWorkbench"
+
     FreeCADGui.addWorkbench(RotaryMoulderWorkbench())
